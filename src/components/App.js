15e2124b9b782501beb4376ad4edd1bb1c75c9f2
mdasad7827 <mohaammadasad1996@gmail.com>
2020-11-10 18:52:05 +0530

    removed p

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <h1 data-ns-test="project-name">Tic Tac Toe</h1>
-    <p data-ns-test="project-description">Tic-tac-toe American English, noughts and crosses Commonwealth English, or Xs and Os, is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid. The player who succeeds in placing three of their marks in a horizontal, vertical, or diagonal row is the winner. It is a solved game with a forced draw assuming best play from both players.</p>
+    
   )
 }
 
